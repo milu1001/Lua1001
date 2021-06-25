4ad743e3599b812ae31e25aab7f79b5be7b3d777
Dawia Sadoune <dasa1001@ds.fh-kl.de>
2021-06-25 04:45:49 +0200

    update

---
+++ Lua_The_Game_Animation/Assets/Scripts/ActivateFindKey.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ActivateFindKey : MonoBehaviour
+{
+    public GameObject ListenBox;
+    public bool PlayerInRange;
+
+// Start is called before the first frame update
+void Start()
+{
+
+}
+
+
+
+public void Update()
+{
+
+    if (Input.GetKeyDown(KeyCode.C) && PlayerInRange)
+    {
+        if (!ListenBox.activeInHierarchy)
+            {
+                ListenBox.SetActive(true);
+            }
+    }
+
+
+}
+
+
+
+private void OnTriggerEnter2D(Collider2D collision)
+{
+    if (collision.CompareTag("Player"))
+    {
+        PlayerInRange = true;
+       
+    }
+}
+
+private void OnTriggerExit2D(Collider2D collision)
+{
+    if (collision.CompareTag("Player"))
+    {
+        PlayerInRange = false;
+       
+    }
+}
+
+}
---
+++ Lua_The_Game_Animation/Assets/Scripts/dialogueTriggerwoText.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class dialogueTriggerwoText : MonoBehaviour
+{
+    public GameObject DialogueBox;
+    public GameObject ListenBox;
+    public bool movement = true;
+    private Character2DController cc;
+    private Animator ani;
+    public bool PlayerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GameObject.FindGameObjectWithTag("Player").GetComponent<Character2DController>();
+        ani = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Animator>();
+    }
+
+
+
+    public void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C) && PlayerInRange)
+        {
+            if (!DialogueBox.activeInHierarchy)
+            {
+                Debug.Log("key");
+                DialogueBox.SetActive(true);
+                ListenBox.SetActive(false);
+                cc.enabled = false;
+                ani.SetBool("isrunning", false);
+            }
+            else if (DialogueBox.activeInHierarchy)
+            {
+                Destroy(ListenBox); ListenBox = null; 
+                Destroy(DialogueBox); DialogueBox = null; 
+                cc.enabled = true;
+            }
+        }
+        
+
+
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerInRange = true;
+            ListenBox.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerInRange = false;
+            ListenBox.SetActive(false);
+        }
+    }
+
+}
