15946caa4ae82a015bd0e83e7ce48e9959b23ac4
Dawia Sadoune <dasa1001@ds.fh-kl.de>
2021-07-06 05:46:17 +0200

    hggvk

--- Lua_The_Game_Animation/Assets/FadeToBlack.cs
+++ Lua_The_Game_Animation/Assets/FadeToBlack.cs
@@ -1,22 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FadeToBlack : MonoBehaviour
 {
     public Animator Black;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Black.speed = 0;
-    }
-
-    // Update is called once per frame
+   
     void Update()
     {
         if (Input.anyKey)
         {
-            Black.speed = 1;
+            FadeToLevel(1);
         }
+    }
+
+    public void FadeToLevel (int levelIndex)
+    {
+        Black.SetTrigger("FadeOut");
     }
 }
--- Lua_The_Game_Animation/Assets/MainMenu.cs
+++ Lua_The_Game_Animation/Assets/MainMenu.cs
@@ -5,11 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    void Update()
+    {
+        if(Input.anyKey)
+        {
+            SceneManager.LoadScene("ActualMenu", LoadSceneMode.Single);
+        }
+    }
     public void PlayGame ()
     {
         Debug.Log("funz)");
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        SceneManager.LoadScene("ActualMenu", LoadSceneMode.Single);
     }
 
     public void QuitGame ()
